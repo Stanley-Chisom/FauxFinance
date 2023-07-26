ef6e609f7868d44a5a7f4cf458a80606afc7f692
Stanley Chisom <stanley.chisom042@gmail.com>
2023-07-26 18:33:52 +0100

    code refactoring improved readbility and modularity

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
+// Utility functions
+function calculateBalance(movements) {
+  return movements.reduce((acc, currVal) => acc + currVal, 0);
+}
+
+function calculateCashIn(movements) {
+  return movements
+    .filter((movement) => movement > 0)
+    .reduce((acc, movement) => acc + movement, 0);
+}
+
+function calculateCashOut(movements) {
+  return movements
+    .filter((movement) => movement < 0)
+    .reduce((acc, movement) => acc + movement, 0);
+}
+
+function calculateInterest(movements) {
+  return movements
+    .filter((movement) => movement > 0)
+    .map((deposit) => (deposit * 1.2) / 100)
+    .reduce((acc, interest) => acc + interest, 0);
+}
+
+// Function to display movements
+function displayMovements(movements) {
+  elements.containerMovements.innerHTML = ""; // Clear the container before displaying
+
+  movements.forEach((movement, i) => {
+    const type = movement > 0 ? "deposit" : "withdrawal";
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${movement}</div>
+      </div>
+    `;
+
+    elements.containerMovements.insertAdjacentHTML("beforeend", html);
+  });
+}
+
+// Function to set balance
+function setBalance(balance) {
+  elements.labelBalance.textContent = `${balance} EUR`;
+}
+
+// Function to set summary values
+function setSummary(cashIn, cashOut, interest) {
+  elements.labelSumIn.textContent = `${cashIn}€`;
+  elements.labelSumOut.textContent = `${Math.abs(cashOut)}€`;
+  elements.labelSumInterest.textContent = `${interest}`;
+}
+
+// Assuming the following accounts and movements exist
 const account1 = {
   owner: "Stanley Chisom",
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
@@ -5,113 +62,51 @@ const account1 = {
   pin: 1111,
 };
 
-const account2 = {
-  owner: "Onwuka Rosario",
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+// Grouping elements by category using the functions
+const elements = {
+  labelWelcome: getElement(".welcome"),
+  labelDate: getElement(".date"),
+  labelBalance: getElement(".balance__value"),
+  labelSumIn: getElement(".summary__value--in"),
+  labelSumOut: getElement(".summary__value--out"),
+  labelSumInterest: getElement(".summary__value--interest"),
+  labelTimer: getElement(".timer"),
+  containerApp: getElement(".app"),
+  containerMovements: getElement(".movements"),
+  btnLogin: getElement(".login__btn"),
+  btnTransfer: getElement(".form__btn--transfer"),
+  btnLoan: getElement(".form__btn--loan"),
+  btnClose: getElement(".form__btn--close"),
+  btnSort: getElement(".btn--sort"),
+  inputLoginUsername: getElement(".login__input--user"),
+  inputLoginPin: getElement(".login__input--pin"),
+  inputTransferTo: getElement(".form__input--to"),
+  inputTransferAmount: getElement(".form__input--amount"),
+  inputLoanAmount: getElement(".form__input--loan-amount"),
+  inputCloseUsername: getElement(".form__input--user"),
+  inputClosePin: getElement(".form__input--pin"),
 };
 
-const account3 = {
-  owner: "Njoku Divine",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
+// Function to query a single element
+function getElement(selector) {
+  return document.querySelector(selector);
+}
 
-const account4 = {
-  owner: "Nwafor Bernard",
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
+// Function to query multiple elements matching a selector
+function getElements(selector) {
+  return document.querySelectorAll(selector);
+}
 
-const accounts = [account1, account2, account3, account4];
+// Calculate and display the balance and summary
+const { movements } = account1;
+const balance = calculateBalance(movements);
+setBalance(balance);
 
-const labelWelcome = document.querySelector(".welcome");
-const labelDate = document.querySelector(".date");
-const labelBalance = document.querySelector(".balance__value");
-const labelSumIn = document.querySelector(".summary__value--in");
-const labelSumOut = document.querySelector(".summary__value--out");
-const labelSumInterest = document.querySelector(".summary__value--interest");
-const labelTimer = document.querySelector(".timer");
+const cashIn = calculateCashIn(movements);
+const cashOut = calculateCashOut(movements);
+const interest = calculateInterest(movements);
+setSummary(cashIn, cashOut, interest);
 
-const containerApp = document.querySelector(".app");
-const containerMovements = document.querySelector(".movements");
+// Display movements
+displayMovements(movements);
 
-const btnLogin = document.querySelector(".login__btn");
-const btnTransfer = document.querySelector(".form__btn--transfer");
-const btnLoan = document.querySelector(".form__btn--loan");
-const btnClose = document.querySelector(".form__btn--close");
-const btnSort = document.querySelector(".btn--sort");
-
-const inputLoginUsername = document.querySelector(".login__input--user");
-const inputLoginPin = document.querySelector(".login__input--pin");
-const inputTransferTo = document.querySelector(".form__input--to");
-const inputTransferAmount = document.querySelector(".form__input--amount");
-const inputLoanAmount = document.querySelector(".form__input--loan-amount");
-const inputCloseUsername = document.querySelector(".form__input--user");
-const inputClosePin = document.querySelector(".form__input--pin");
-
-const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-const displayMovements = (movements) => {
-  movements.forEach((movement, i) => {
-    const type = movement > 0 ? "deposit" : "withdrawal";
-
-    const html = `
-    <div class="movements__row">
-    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-    <div class="movements__value">${movement}</div>
-  </div>
-    `;
-    containerMovements.insertAdjacentHTML("afterbegin", html);
-  });
-};
-
-displayMovements(account1.movements);
-
-const userName = (accounts) => {
-  accounts.forEach((account) => {
-    account.username = account.owner
-      .toLowerCase()
-      .split(" ")
-      .map((name) => name[0])
-      .join("");
-  });
-};
-
-const showBalance = (movements) => {
-  const balance = movements.reduce((acc, currVal) => acc + currVal, 0);
-  labelBalance.textContent = `${balance} EUR`;
-};
-showBalance(account1.movements);
-
-const showBalanceSummary = (movements) => {
-  const cashIn = movements
-    .filter((movement) => movement > 0)
-    .reduce((acc, movement) => acc + movement, 0);
-  labelSumIn.textContent = `${cashIn}€`;
-
-  const cashOut = movements
-    .filter((movement) => movement < 0)
-    .reduce((acc, movement) => acc + movement, 0);
-  labelSumOut.textContent = `${Math.abs(cashOut)}€`;
-
-  const interest = movements
-    .filter((movement) => movement > 0)
-    .map((deposit) => (deposit * 1.2) / 100)
-    .reduce((acc, interest) => acc + interest, 0);
-  labelSumInterest.textContent = `${interest}`;
-};
-showBalanceSummary(account1.movements);
-
-const deposits = movements.filter((movement) => movement > 0);
-
-const withdrawal = movements.filter((movement) => movement < 0);
